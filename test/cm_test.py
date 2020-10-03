96470c9892cdd4a5646e1209eaecd764a42da789
xyy-jiaer <61101265+xyy-jiaer@users.noreply.github.com>
2020-10-03 12:17:25 +0800

    Update cm_test.py

--- test/cm_test.py
+++ test/cm_test.py
@@ -14,13 +14,13 @@ class IAppium(unittest.TestCase):
         desired_caps = {}
         appium_server_url = config['appium_server_url']
         desired_caps['platformName'] = config['desired_caps']['platformName']
-        desired_caps['udid'] = config['desired_caps']['udid']
+#         desired_caps['udid'] = config['desired_caps']['udid']
         desired_caps['deviceName'] = config['desired_caps']['deviceName']
         desired_caps['appPackage'] = config['desired_caps']['appPackage']
         desired_caps['appActivity'] = config['desired_caps']['appActivity']
         desired_caps['automationName'] = config['desired_caps']['automationName']
         desired_caps['noReset'] = config['desired_caps']['noReset']
-        desired_caps['app'] = f'{os.path.abspath(os.curdir)}/app/ContactManager.apk'
+#         desired_caps['app'] = f'{os.path.abspath(os.curdir)}/app/ContactManager.apk'
 
         self.driver = webdriver.Remote(appium_server_url, desired_caps)
 
